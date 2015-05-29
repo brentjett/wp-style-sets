e1708d9c901803f291d6cf09dc7316dd18f40720
Brent Jett <brentjett@gmail.com>
2015-05-29 14:00:56 -0500

    wrap in manager class

--- index.php
+++ index.php
@@ -18,28 +18,43 @@ if ( file_exists( __DIR__ . '/vendor/leafo/scss/scss.inc.php' ) ) {
 }
 
 add_action('init', function() {
-
+    if (class_exists('WP_StyleSetManager')) {
+        $GLOBALS['wp_style_set_manager'] = new WP_StyleSetManager();
+    }
 });
-$wp_style_sets = array();
 
-// add new set
-function register_styleset($handle, $args = array()) {
-    global $wp_style_sets;
+class WP_StyleSetManager {
 
-    $wp_style_sets[$handle] = $args;
-}
+    public $style_sets = array();
 
+    function __construct() {
 
-// add item to set
-function add_style_unit_to_set($unit, $set) {
-    global $wp_style_sets;
-    $wp_style_sets[$set] = $unit;
-}
+    }
 
-// render set
-function render_styleset($handle) {
-    $units = apply_filters('wp_stylesets/pre_render/units', $units);
-    // returns css? or if file type, url to cached file?
+    public function get_style_sets() {
+        return apply_filters('wp_stylesets/pre_get', $this->style_sets);
+    }
+
+    // add new set
+    public function register_styleset($handle, $args = array()) {
+        global $wp_style_sets;
+
+        $wp_style_sets[$handle] = $args;
+    }
+
+    // add item to set
+    public function add_style_unit_to_set($unit, $set) {
+        global $wp_style_sets;
+        $wp_style_sets[$set] = $unit;
+    }
+
+    // render set
+    public function render_styleset($handle) {
+        $units = $wp_style_sets
+        $units = apply_filters('wp_stylesets/pre_render/units', $units);
+        // returns css? or if file type, url to cached file?
+    }
+
 }
 
 ?>

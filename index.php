b6d2408476d128c5ae7f3449f5ebe3476714255a
Brent Jett <brentjett@gmail.com>
2015-05-29 13:50:07 -0500

    Add SASS compiler. Initial API design

--- index.php
+++ index.php
@@ -6,22 +6,38 @@ Author: Brent Jett
 Description: A concept plugin for creating a set of styles to be compiled and output as either a file or <style> block.
 */
 
-// Load Less Compiler
+// Load LESS Compiler
 if ( file_exists( __DIR__ . '/vendor/leafo/lessphp/lessc.inc.php' ) ) {
     // load LESS parser
     require_once( __DIR__ . '/vendor/leafo/lessphp/lessc.inc.php' );
+}
+// Load SASS Compiler
+if ( file_exists( __DIR__ . '/vendor/leafo/scss/scss.inc.php' ) ) {
+    // load LESS parser
+    require_once( __DIR__ . '/vendor/leafo/scss/scss.inc.php' );
 }
 
 add_action('init', function() {
 
 });
-$brj_style_sets = array();
+$wp_style_sets = array();
 
 // add new set
+function register_styleset($handle, $args = array()) {
+    global $wp_style_sets;
+
+    $wp_style_sets[$handle] = $args;
+}
+
+
 // add item to set
+function add_style_unit_to_set($unit, $set) {
+    global $wp_style_sets;
+    $wp_style_sets[$set] = $unit;
+}
 
 // render set
-function render_styleset_for_name($name) {
+function render_styleset($handle) {
     $units = apply_filters('wp_stylesets/pre_render/units', $units);
     // returns css? or if file type, url to cached file?
 }
